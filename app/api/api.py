b5fa525111757690265f53d881bd95209ac7af48
Alice Huang <viento.a13@gmail.com>
2022-03-03 19:52:19 +0800

    Fixed nextPage, changed host and response method.

--- app/api/api.py
+++ app/api/api.py
@@ -1,7 +1,6 @@
-import json
-
 from flask import request
-from flask import Response
+from flask import make_response
+from flask import jsonify
 
 from . import api_
 from .. import db
@@ -18,23 +17,36 @@ def show_attractions():
         if keyword:
             sql = ("SELECT name, category, description, address, transport, mrt, latitude, longitude, images FROM spots WHERE name LIKE %s LIMIT %s, %s")
             sql_data = ("%" + keyword + "%", item_start_n - 1, 12)
+
+            # Check next page if exists
+            count_sql = ("SELECT id FROM spots WHERE name LIKE %s ORDER BY id LIMIT %s, %s")
+            count_sql_data = ("%" + keyword + "%", item_start_n + 11, 1)
+
         else:
             sql = ("SELECT name, category, description, address, transport, mrt, latitude, longitude, images FROM spots LIMIT %s, %s")
             sql_data = (item_start_n - 1, 12)
 
+            # Check next page if exists
+            count_sql = ("SELECT id FROM spots ORDER BY id LIMIT %s, %s")
+            count_sql_data = (item_start_n + 11, 1)
+
         results = db.execute_sql(sql, sql_data, "all")
         for result in results:
             result["images"] = result["images"].split(",")
+
+        # Check next page if exists
+        count = db.execute_sql(count_sql, count_sql_data, "one")
         
-        if results:
-            result_dict = {"nextPage": int(page) + 1, "data": results}
+        if count:
+            result_dict = {"nextPage": page + 1, "data": results}
         else:
             result_dict = {"nextPage": None, "data": results}
 
-        return json.dumps(result_dict)
+        return jsonify(result_dict)
+
     else:
         result_dict = {"error": True, "message": "Page requested invalid"}
-        return Response(json.dumps(result_dict), status = "400")
+        return make_response(jsonify(result_dict), 400)
         
 
 @api_.route("/attraction/<attractionId>", methods = ["GET"])
@@ -45,13 +57,13 @@ def show_single_attraction(attractionId):
     results = db.execute_sql(sql, sql_data, "one")
     if results:
         result_dict = {"data": results}
-        return json.dumps(result_dict)
+        return jsonify(result_dict)
     else:
         result_dict = {"error": True, "message": "Attraction id requested invalid"}
-        return Response(json.dumps(result_dict), status = "400")
+        return make_response(jsonify(result_dict), 400)
 
 @api_.app_errorhandler(500)
 def handle_500(err):
     result_dict = {"error": True, "message": "Internal server error"}
-    return Response(json.dumps(result_dict), status = "500")
+    return make_response(jsonify(result_dict), 500)
     
