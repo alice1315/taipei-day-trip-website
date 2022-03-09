2bea576dbbbbcf6e40fd57f0041ce6667314a196
Alice Huang <viento.a13@gmail.com>
2022-03-10 01:20:05 +0800

    Finished infinite scroll of index.html.

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,14 +1,15 @@
-function init(){
-    let reqUrl = "/api/attractions?page=0";
+let init = () => {
+    let reqUrl = `/api/attractions?page=0`;
 
-    fetch(reqUrl).then(function(resp){
-        return resp.json();
-    }).then(function(datas){
+    fetch(reqUrl)
+    .then(resp => resp.json())
+    .then(function(datas){
         getSpots(datas);
+        infiniteScroll(datas);
     })
 }
 
-function getSpots(jsonObj){
+let getSpots = (jsonObj) => {
     let spots = jsonObj["data"];
 
     for (i = 0; i < spots.length; i++){
@@ -41,3 +42,48 @@ function getSpots(jsonObj){
         spotInfo.appendChild(spotCat);
     }
 }
+
+let infiniteScroll = (jsonObj) => {
+    let loadingObserver = document.querySelector(".observer");
+    let page = jsonObj["nextPage"];
+
+    let fetchPage = () => {
+        if (page !== null){
+            let reqUrl = `/api/attractions?page=${page}`;
+            fetch(reqUrl)
+            .then(resp => resp.json())
+            .then(function(datas){
+                getSpots(datas);
+                page = datas["nextPage"];
+            })
+        } else{
+            observer.unobserve(loadingObserver);
+        }
+    }
+    
+    let callback = (entries) => {
+        if (entries[0].isIntersecting){
+            setTimeout(fetchPage, 3000);
+        }
+    }
+
+    let observer = new IntersectionObserver(callback);
+
+    observer.observe(loadingObserver);
+}
+
+// let getSearchSpots = () => {
+//     let searchWord = document.getElementById("search-word").value;
+//     let reqUrl = `/api/attractions?page=0&keyword=${searchWord}`;
+
+//     let content = document.getElementById("content");
+
+//     fetch(reqUrl)
+//     .then(resp => resp.json())
+//     .then(function(datas){
+//         content.innerHTML = "";
+//         getSpots(datas);
+//         infiniteScroll(datas);
+//     })
+//     return false;
+// }
